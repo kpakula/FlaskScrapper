750ce6e02d7ac587c36bad6ffd3af54caae6b6bb
Pakulaa <Konrad.Pakula@Live.umcs.edu.pl>
2018-05-21 23:42:57 +0200

    Added site scraping and saving into CSV file / Added .gitignore

--- scrap.py
+++ scrap.py
@@ -1,24 +1,36 @@
 from urllib.request import urlopen
 from urllib.request import Request
 from bs4 import BeautifulSoup as soup
+import re
 
-user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
-my_url = 'https://steamdb.info/sales/'
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 " \
+             "Safari/537.36 "
+my_url = 'https://steamdb.info/sales/?min_discount=90&min_rating=0'
+
+regex = re.compile(r'app appimg.*')
+
 
 client = Request(my_url, headers={"User-Agent": user_agent})
 page_html = urlopen(client).read()
 
 page_soup = soup(page_html, "html.parser")
+containers = page_soup.findAll("tr", {"class": regex})
 
-containers = page_soup.findAll("tr", {"class": "app appimg"})
+filename = "discount.csv"
+f = open(filename, "w")
+
+headers = "title, price, rating, discount, link_steam\n"
+f.write(headers)
+
 
 for container in containers:
-    link_steam = container.a
-    game_title = container.find("a", {"class": "b"})
-    price = container.find("td", {"class": "price-discount"})
+    link_steam = container.a['href']
+    game_title = container.find("a", {"class": "b"}).text
+    discount = '90%'
+    info_container = container.findAll("td")
+    price = info_container[4].text
+    rating = info_container[5].text
 
-    # print(link_steam)
-    # print(game_title)
-    # print(price)
+    f.write(game_title.replace(",", ".") + "," + price + "," + rating + "," + discount + "," + link_steam + "\n")
 
-
+f.close()

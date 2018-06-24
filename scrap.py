b7fe89485cc91f83dee8f3f190e6af502b15896b
Pakulaa <Konrad.Pakula@Live.umcs.edu.pl>
2018-06-24 12:20:48 +0200

    Some changes in scrap file

--- scrap.py
+++ scrap.py
@@ -16,44 +16,54 @@ class Scrap:
         self.filename = "discount.csv"
         self.objects = []
 
+
     def initialization(self):
         client = Request(self.my_url, headers={"User-Agent": self.user_agent})
         page_html = urlopen(client).read()
         page_soup = soup(page_html, "html.parser")
         containers = page_soup.findAll("tr", {"class": self.regex_container})
+        obj = []
+        id = 1;
 
-        try:
-            f = open(self.filename, "w")
+        for container in containers:
+            game = Game()
+            game.set_link_steam(container.a['href'])
+            steam_id_regex = self.regexID.findall(game.get_link_steam())
+            game.set_steam_id((steam_id_regex[0])[1:-1])
+            game.set_game_title(container.find("a", {"class": "b"}).text)
+            game.set_discount('90%')
 
-            headers = "id, title, price, rating, discount, link_steam, steam_id\n"
-            f.write(headers)
+            info_container = container.findAll("td")
 
-            id = 1;
+            game.set_price(info_container[4].text)
+            game.set_rating(info_container[5].text)
+            game.set_id(str(id))
 
-            for container in containers:
-                game = Game()
-                # link_steam = container.a['href']
-                game.set_link_steam(container.a['href'])
-                steam_id_regex = self.regexID.findall(game.get_link_steam())
-                game.set_steam_id((steam_id_regex[0])[1:-1])
-                game.set_game_title(container.find("a", {"class": "b"}).text)
-                game.set_discount('90%')
+            id = id + 1
+            obj.append(game)
+        self.sortbyRating(obj)
+        self.getCSV()
 
-                info_container = container.findAll("td")
-                game.set_price(info_container[4].text)
-                game.set_rating(info_container[5].text)
-                game.set_id(str(id))
+    def sortbyRating(self, obj):
+        obj.sort(key=lambda x: x.rating, reverse=True)
+        game = Game()
+        i = 0
+        while i < 100:
+            game = obj[i]
+            self.objects.append(game)
+            i += 1
 
-                f.write(game.get_id() + "," + game.get_game_title() + "," + game.get_price() + "," + game.get_price() +
-                        "," + game.get_rating() + "," + game.get_link_steam() + "," + game.get_steam_id() + "\n")
-                id = id + 1
-                self.objects.append(game)
-
-            f.close()
-
-        except PermissionError:
-            print("The .csv file is open")
+    def getCSV(self):
+        f = open(self.filename, "w")
+        headers = "id, title, price, rating, discount, link_steam, steam_id\n"
+        f.write(headers)
+        i = 0
+        game = Game()
+        while i < len(self.objects):
+            game = self.objects[i]
+            f.write(game.get_id() + "," + game.get_game_title() + "," + game.get_price() + "," + game.get_rating() +  "," + game.get_discount() + "," + game.get_link_steam() + "," + game.get_steam_id() + "\n")
+            i += 1
+        f.close()
 
     def get_objects(self):
         return self.objects
-
